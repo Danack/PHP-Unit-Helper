7e50284d762b816acde56919f82f3431a6bad4c5
Danack <Danack@basereality.com>
2023-04-09 14:23:30 +0100

    Added expectExceptionMessageRegex

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Small set of functions to use within PHPUnit.
 
 
-[![Actions Status](https://github.com/Danack/PHPUnitHelper/workflows/Tests/badge.svg)](https://github.com/Danack/Params/actions)
+[![Actions Status](https://github.com/Danack/PHPUnitHelper/workflows/Tests/badge.svg)](https://github.com/Danack/PHPUnitHelper/actions)
 
--- src/Danack/PHPUnitHelper/StringTemplateMatching.php
+++ src/Danack/PHPUnitHelper/StringTemplateMatching.php
@@ -27,4 +27,14 @@ trait StringTemplateMatching
         $regexp = templateStringToRegExp($templateString);
         $this->expectExceptionMessageMatches($regexp);
     }
+
+    /**
+     * @param string $text The string to look for.
+     * @param string $flags The regexp flags to use.
+     */
+    public function expectExceptionMessageRegex(string $text, string $flags = 'iu'): void
+    {
+        $regexp = '/' . preg_quote($text, '/') . '/' . $flags;
+        $this->expectExceptionMessageMatches($regexp);
+    }
 }
